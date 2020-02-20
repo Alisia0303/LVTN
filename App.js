8f2faec5c95b460072deb9dcd1bcce234c5d62f6
Alisia0303 <ltth.qh286@gmail.com>
2020-02-20 16:51:54 +0700

    initial creatiton

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import {  Text,  View,} from 'react-native';
+import Hello from './component/demo';
+
+ export default class ViewColoredBoxesWithText extends Component {
+  render() {
+    return <Hello/>
+  }
+}
