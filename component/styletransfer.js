b281da016a55406ec25aae79c9b41f73a4bfcf29
Alisia0303 <ltth.qh286@gmail.com>
2020-02-25 18:05:50 +0700

    add complete UI for edit style

--- component/styletransfer.js
+++ component/styletransfer.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Text,   View, Image} from 'react-native';
+import { Text,   View, Image, StyleSheet, ImageBackground} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 
 export default class StyleTransfer extends Component {
   render() {
@@ -10,31 +13,43 @@ export default class StyleTransfer extends Component {
           <Image 
               style={{flex:1}}
               source={require('./../test.jpg')}
+              style={{width: '100%', height: '100%'}}
           />        
         </View>
-        <View style={{flex:2, backgroundColor:'yellow'}}>
-          <View style={{flex: 1, backgroundColor:'white'}}/>
+        <View style={{flex:2}}>
+          <View style={{flex: 1, backgroundColor:'white', flexDirection:'row'}}>
+            <View style={{flex:1}}>
+                <Icon name="download" size={30} style={[styles.downButton]}  />
+            </View>
+            <View style={{flex:1}}>
+                <Icon name="share-square" size={30} style={[styles.shareButton]}  />
+            </View>
+          </View>
           <View style={{flex: 2, flexDirection:'row'}}>
             <View style={{flex:1}}>
-              <Text>LIBRARIES</Text>
+              <Text style={styles.lib}>LIBRARIES</Text>
             </View>
             <View style={{flex:1}}>
-              <Image
-                style={{flex:1}}
-                source={require('./../snow.jpg')}
-              />
+              <ImageBackground source={require('./../snow.jpg')} style={{width: '100%', height: '100%'}}>
+                <View style={styles.despStyles}>
+                  <Text style={{color:'white'}}>SNOW</Text>
+                </View>
+              </ImageBackground>
+            </View>
+              
+            <View style={{flex:1}}>
+              <ImageBackground source={require('./../monet.jpg')} style={{width: '100%', height: '100%'}}>
+                <View style={styles.despStyles}>
+                  <Text style={{color:'white'}}>PAINTING</Text>
+                </View>
+              </ImageBackground>
             </View>
             <View style={{flex:1}}>
-              <Image
-                style={{flex:1}}
-                source={require('./../monet.jpg')}
-              />
-            </View>
-            <View style={{flex:1}}>
-              <Image
-                style={{flex:1}}
-                source={require('./../tmp.jpg')}
-              />
+              <ImageBackground source={require('./../tmp.jpg')} style={{width: '100%', height: '100%'}}>
+                <View style={styles.despStyles}>
+                  <Text style={{color:'white'}}>MORE</Text>
+                </View>
+              </ImageBackground>
             </View>
           </View>
         </View>
@@ -42,3 +57,28 @@ export default class StyleTransfer extends Component {
     );
   }
 };
+
+const styles=StyleSheet.create({
+  shareButton :{
+    padding:10,
+    position: 'absolute', right: 10,
+  },
+  downButton:{
+    padding:10,
+    position: 'absolute', left: 10,
+  },
+  lib:{
+    fontSize:19,
+    top: 30,
+  },
+  despStyles:{
+    position: 'absolute', 
+    top: 90, 
+    left: 0, 
+    right: 0, 
+    bottom: 0, 
+    justifyContent: 'center', 
+    
+    alignItems: 'center'}
+   
+});
